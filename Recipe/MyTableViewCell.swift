be62c0bb2ef8bc9d9a1c163f542a1198bbdf14ee
Zero-Xiong <zero.xiong@gmail.com>
2016-04-27 17:21:57 +0800

    complete basic function

---
+++ Recipe/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  Recipe
+//
+//  Created by xiong on 27/4/16.
+//  Copyright © 2016 Zero Inc. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Recipe/NSUserDefaultsManager.swift
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaultsManager.swift
+//  Recipe
+//
+//  Created by xiong on 27/4/16.
+//  Copyright © 2016 Zero Inc. All rights reserved.
+//
+
+import UIKit
+
+class NSUserDefaultsManager: NSObject {
+
+}
